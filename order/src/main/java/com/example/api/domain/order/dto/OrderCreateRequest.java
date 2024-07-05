2f29f5363b2aabb979751472b130103d21d522f7
wooyong99 <wulovesk@naver.com>
2024-07-05 18:40:19 +0900

    feat : 주문 Controller 구현 (#5)

---
+++ order/src/main/java/com/example/api/domain/order/dto/OrderCreateRequest.java
@@ -0,0 +1,17 @@
+package com.example.api.domain.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderCreateRequest {
+
+    private Long userId;
+
+    private Long price;
+}
