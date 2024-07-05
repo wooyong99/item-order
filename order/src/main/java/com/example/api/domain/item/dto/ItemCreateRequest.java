fc8e1976958011082227fd4f0dcab6cab21bd06e
wooyong99 <wulovesk@naver.com>
2024-07-05 18:30:38 +0900

    feat : 아이템 Controller 구현 (#3)

---
+++ order/src/main/java/com/example/api/domain/item/dto/ItemCreateRequest.java
@@ -0,0 +1,33 @@
+package com.example.api.domain.item.dto;
+
+
+import com.example.core.domain.item.domain.Item;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemCreateRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private Long price;
+
+    @NotBlank
+    private Long stock;
+
+    public Item toEntity() {
+        return Item.builder()
+                .name(name)
+                .price(price)
+                .stock(stock)
+                .build();
+    }
+}
