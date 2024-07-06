47f9d087f9a494654ace24cda1abf75d166d22e4
wooyong99 <wulovesk@naver.com>
2024-07-06 09:23:58 +0900

    refactor : 비관적 락 사용 (#15)

--- core/src/main/java/com/example/core/domain/item/repository/ItemRepository.java
+++ core/src/main/java/com/example/core/domain/item/repository/ItemRepository.java
@@ -1,7 +1,16 @@
 package com.example.core.domain.item.repository;
 
 import com.example.core.domain.item.domain.Item;
+import jakarta.persistence.LockModeType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemCustomRepository {
+
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    @Query("select i from Item i where i.id = :itemId")
+    Optional<Item> findByIdWithPessimisticLock(@Param("itemId") Long itemId);
 }
